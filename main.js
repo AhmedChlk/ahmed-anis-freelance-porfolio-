dded3ccf400894f954d60c4c514ce1c5d75397ae
Choulak Ahmed <ahmedchoulak2004@gmail.com>
2025-07-27 21:59:58 +0200

    project  section

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 const htmlEl = document.documentElement;
 const btnHamb = document.querySelector(".hamburger");
-const drawer = document.querySelector(".drawer-top");
+const drawer = document.getElementById("mobileMenu");
 const scrim = document.querySelector(".scrim");
 const btnClose = document.querySelector(".drawer-close");
-let lastFocused;
+let lastFocused = null;
 
-/**
- * Ouvre/ferme le drawer en fullscreen,
- * gère aria-attributes, scroll-lock et focus-trap.
- */
 function toggleDrawer(open) {
   htmlEl.classList.toggle("drawer-open", open);
   btnHamb.setAttribute("aria-expanded", open);
@@ -17,13 +13,12 @@ function toggleDrawer(open) {
 
   if (open) {
     lastFocused = document.activeElement;
-    drawer.querySelector("a,button")?.focus();
+    drawer.querySelector("a,button").focus();
   } else {
     lastFocused?.focus();
   }
 }
 
-// Événements d’ouverture/fermeture
 btnHamb.addEventListener("click", () => toggleDrawer(true));
 btnClose.addEventListener("click", () => toggleDrawer(false));
 scrim.addEventListener("click", () => toggleDrawer(false));
